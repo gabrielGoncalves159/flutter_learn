6e3d94adc78a8f98a5d4e1098c848da36e72c3ac
Gabriel Gonçalves <ga.goncalves159753@gmail.com>
2023-09-19 21:26:29 -0300

    aprimorando aplicativo

--- app_flutter/lib/main.dart
+++ app_flutter/lib/main.dart
@@ -17,26 +17,20 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
+          leading: Container(), //cria um espaço a esquerda pode ser utilizado pra espaço ou pra colocar um icone e etc
           title: const Text(
-            'Flutter: Meus primeiros passos',
+            'Tarefas',
             style: TextStyle(color: Colors.white),
           ),
           backgroundColor: Colors.blueAccent,
         ),
         body: ListView(
           children: [
-            Task('Aprender Flutter sfsdfsdfsdf dfsdfsdfsdfs fsdfsdfsdf sdf'),
-            Task('Tocar guitarra'),
-            Task('Meditar'),
-            Task('Aprender Flutter'),
-            Task('Tocar guitarra'),
-            Task('Meditar'),
-            Task('Aprender Flutter'),
-            Task('Tocar guitarra'),
-            Task('Meditar'),
-            Task('Aprender Flutter'),
-            Task('Tocar guitarra'),
-            Task('Meditar'),
+            Task('Aprender Flutter', 'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large', 0),
+            Task('Tocar guitarra', 'https://akamai.sscdn.co/gcs/cifra-blog/pt/wp-content/uploads/2022/03/5730533-tocando-guitarra-rapido.jpg', 3),
+            Task('Meditar', 'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg', 5),
+            Task('Aprender teste', 'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large', 2),
+            Task('Aprender Aprender', 'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large', 4),
           ],
         ),
         floatingActionButton: FloatingActionButton(
@@ -46,13 +40,22 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Task extends StatelessWidget {
+class Task extends StatefulWidget {
   final String nome;
-  const Task(this.nome, {Key? key}) : super(key: key);
+  final String foto;
+  final int dificuldade;
+
+  const Task(this.nome, this.foto, this.dificuldade, {Key? key}) : super(key: key);
 
   @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 0;
+  
+  @override
   Widget build(BuildContext context) {
-    int nivel = 0;
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
@@ -74,31 +77,101 @@ class Task extends StatelessWidget {
                         color: Colors.black26,
                         width: 72,
                         height: 100,
+                        child: Image.network(
+                        widget.foto,
+                        fit: BoxFit.cover,
+                      ),
+
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 200,
+                            child: Text(
+                              widget.nome,
+                              style: const TextStyle(
+                                fontSize: 24,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ),
+                          Row(children: [
+                            Icon(
+                              Icons.star, 
+                              size: 15, 
+                              color: (widget.dificuldade >= 1) ? Colors.blue : Colors.blue[100]
+                            ),
+                            Icon(
+                              Icons.star, 
+                              size: 15, 
+                              color: (widget.dificuldade >= 2) ? Colors.blue : Colors.blue[100]
+                            ),
+                            Icon(
+                              Icons.star, 
+                              size: 15, 
+                              color: (widget.dificuldade >= 3) ? Colors.blue : Colors.blue[100]
+                            ),
+                            Icon(
+                              Icons.star, 
+                              size: 15, 
+                              color: (widget.dificuldade >= 4) ? Colors.blue : Colors.blue[100]
+                            ),
+                            Icon(
+                              Icons.star, 
+                              size: 15, 
+                              color: (widget.dificuldade >= 5) ? Colors.blue : Colors.blue[100]
+                            ),
+                          ],),
+                        ],
                       ),
                       Container(
-                        width: 200,
-                        child: Text(
-                          nome,
-                          style: const TextStyle(
-                            fontSize: 24,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                      ),
-                      ElevatedButton(
-                          onPressed: () {
-                            nivel++;
-                          },
-                          child: const Icon(Icons.arrow_drop_up))
+                        width: 68,
+                        height: 90,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                nivel++;
+                              });
+                            },
+                            child: const Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Icon(Icons.arrow_drop_up),
+                                Text("UP", style: TextStyle(fontSize: 10),),
+                              ],
+                            ),
+                            ),
+                      )
                     ],
                   ),
                 ),
-                Text(
-                  'Nivel $nivel',
-                  style: const TextStyle(
-                    fontSize: 16,
-                    color: Colors.white,
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: LinearProgressIndicator(
+                          color: Colors.white,
+                          value: (widget.dificuldade > 0) ? (nivel/widget.dificuldade)/10 : 1,
+                        ),
+                        width: 200,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Text(
+                        'Nivel $nivel',
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
